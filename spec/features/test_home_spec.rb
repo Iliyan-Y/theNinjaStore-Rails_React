e475e6eccff785fad29ffe0d9f297942f4eef811
Iliyan-Y <maximizebg@gmail.com>
2020-09-29 23:31:29 +0100

    React connected to the rails view

--- spec/features/test_home_spec.rb
+++ spec/features/test_home_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature "Photo", type: :feature do
   scenario "Visit the home page and see react set up"do 
     visit("/")
-    expect(page).to have_content("Hello React")
+    expect(page).to have_content("")
   end
 end
