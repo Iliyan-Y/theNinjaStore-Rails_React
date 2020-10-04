dc609fa4c93b101c279ac8a541fba5c59dcc8b22
Iliyan-Y <maximizebg@gmail.com>
2020-10-04 23:13:46 +0100

    React routes added

--- app/javascript/packs/components/app.jsx
+++ app/javascript/packs/components/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Home from './storeHome/home';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
   );
 };
 
