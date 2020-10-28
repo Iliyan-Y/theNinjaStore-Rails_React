400a7c851b49ff79e14181820a2718835220174e
Iliyan-Y <maximizebg@gmail.com>
2020-10-28 22:56:37 +0000

    Mocking redux hooks succsessful

--- spec/javascript/packs/components/storeHome/home_spec.spec.js
+++ spec/javascript/packs/components/storeHome/home_spec.spec.js
@@ -3,30 +3,49 @@ import ReactDOM from 'react-dom';
 import Home from 'packs/components/storeHome/home';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import { useSelector } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useSelector: jest.fn(),
+}));
 
 describe('HelloReact component', () => {
-  const initialState = {
-    id: '',
-    all: [
+  it('do something', () => {
+    const initialState = [
       {
-        id: 'fake id',
-        name: 'fake name',
-        description: 'fake item',
-        image: 'no Image yet',
-        created: 'forever',
+        id: '',
+        all: [
+          {
+            id: 'fake id',
+            name: 'fake name',
+            description: 'fake item',
+            image: 'no Image yet',
+            created: 'forever',
+          },
+        ],
       },
-    ],
-  };
-  const mockStore = configureStore();
-  let store = mockStore(initialState);
-  console.log(store.getState());
-  const root = document.createElement('div');
-  ReactDOM.render(
-    <Provider store={store}>
-      <Home />
-    </Provider>,
-    root
-  );
+    ];
 
-  expect(root.querySelector('p').textContent).toBe('Home Page');
+    //mockstate
+    const mockStore = configureStore();
+    let store = mockStore(initialState);
+
+    useSelector.mockImplementation(() => initialState);
+
+    //render
+    const root = document.createElement('div');
+    ReactDOM.render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      </BrowserRouter>,
+      root
+    );
+
+    //expected
+    expect(root.querySelector('p').textContent).toBe('Home Page');
+  });
 });
