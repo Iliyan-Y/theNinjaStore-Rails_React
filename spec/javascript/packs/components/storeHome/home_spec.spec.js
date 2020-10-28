d26f01ced9f4807e7d86db212ddbcd6570dd2895
Iliyan-Y <maximizebg@gmail.com>
2020-10-28 19:55:43 +0000

    enzyme changed with react testing library

--- spec/javascript/packs/components/storeHome/home_spec.spec.js
+++ spec/javascript/packs/components/storeHome/home_spec.spec.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import ReactDOM from 'react-dom';
 import Home from 'packs/components/storeHome/home';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 describe('HelloReact component', () => {
-  const container = shallow(<Home />);
-  it('should match the snapshot', () => {
-    expect(container.html()).toMatchSnapshot();
-  });
+  const initialState = {
+    id: '',
+    all: [
+      {
+        id: 'fake id',
+        name: 'fake name',
+        description: 'fake item',
+        image: 'no Image yet',
+        created: 'forever',
+      },
+    ],
+  };
+  const mockStore = configureStore();
+  let store = mockStore(initialState);
+  console.log(store.getState());
+  const root = document.createElement('div');
+  ReactDOM.render(
+    <Provider store={store}>
+      <Home />
+    </Provider>,
+    root
+  );
+
+  expect(root.querySelector('p').textContent).toBe('Home Page');
 });
