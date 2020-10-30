6e6be4bb8d90277297d8b06d0f1f83ee21eededd
Iliyan-Y <ili.yankov91@gmail.com>
2020-10-30 19:11:19 +0000

    Token field added to the user table

--- db/migrate/20201030173254_devise_create_users.rb
+++ db/migrate/20201030173254_devise_create_users.rb
@@ -34,6 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
 
       t.timestamps null: false
+      t.string :auth_token
     end
 
     add_index :users, :email,                unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_10_30_173254) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "auth_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
