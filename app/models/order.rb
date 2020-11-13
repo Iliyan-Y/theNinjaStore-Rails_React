1817d1f9454db72f9dddce84aeb16accc2db3a4f
Iliyan-Y <ili.yankov91@gmail.com>
2020-11-13 17:28:10 +0000

    Order table updated to have products as array

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :user
+  #validates :products, :presence => true
 end
--- db/migrate/20201113170501_create_orders.rb
+++ db/migrate/20201113170501_create_orders.rb
@@ -2,8 +2,10 @@ class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
+      t.string 'products', array: true
 
       t.timestamps
     end
+    add_index :orders, :products, using: 'gin'
   end
 end
