78a990e8f7802f210e833dff051ed564b3a57b42
Iliyan-Y <ili.yankov91@gmail.com>
2020-11-15 19:36:45 +0000

    Orders controller refactured

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -11,22 +11,6 @@ class Api::V1::OrdersController < ActionController::API
 
   protected
 
-  def create_order
-    order = @user.orders.create(products: params['order']['productsId'])
-    if order.save 
-      render json: order, status: 200
-    else 
-      head 400
-    end
-  end
-
-  def find_user 
-    token = User.decode(params['order']['token'])
-    return false unless token
-
-    @user = User.find_by_email(token['user'])
-  end
-
   def order_params
     params.require(:order).permit(:email, :customer_name, :address, :phone, :post_code, :productsId => [] )
   end
