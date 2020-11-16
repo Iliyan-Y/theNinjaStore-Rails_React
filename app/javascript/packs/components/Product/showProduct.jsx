47e20d1d70909c8fc0d139b8c85e3344584447bc
Iliyan-Y <ili.yankov91@gmail.com>
2020-11-16 16:48:55 +0000

    show product component updated to hide edit and delete options

--- app/javascript/packs/components/Product/showProduct.jsx
+++ app/javascript/packs/components/Product/showProduct.jsx
@@ -2,21 +2,48 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DeleteProduct from './deleteProduct';
 import EditProduct from './editProduct';
+import { useCookies } from 'react-cookie';
 
 const ShowProduct = ({ match }) => {
   const {
     params: { productId },
   } = match;
   let [product, setProduct] = useState();
+  const [cookies] = useCookies();
+  let [isUser, setIsUser] = useState(false);
 
   useEffect(() => {
     axios
       .get('/api/v1/products/' + productId)
       .then((res) => setProduct(res.data))
       .catch((err) => console.error(err.message));
+
+    checkForUser();
   }, []);
 
-  function renderProducts() {
+  let checkForUser = () => {
+    let token = cookies.user_token;
+    axios
+      .get('/api/v1/users/verify', {
+        headers: {
+          'token': token,
+        },
+      })
+      .then((res) => (res.status == 200 ? setIsUser(true) : ''))
+      .catch((err) => console.error(err.message));
+  };
+
+  let renderOptions = () => {
+    if (isUser)
+      return (
+        <div>
+          <DeleteProduct productId={productId} />
+          <EditProduct product={product} />
+        </div>
+      );
+  };
+
+  let renderProducts = () => {
     if (product)
       return (
         <>
@@ -24,13 +51,10 @@ const ShowProduct = ({ match }) => {
           <p>{product.description}</p>
           <p>£{product.price}</p>
           <img src={product.image} alt="" style={{ width: '250px' }} />
-          <div>
-            <DeleteProduct productId={productId} />
-            <EditProduct product={product} />
-          </div>
+          {renderOptions()}
         </>
       );
-  }
+  };
   return <span>{renderProducts()}</span>;
 };
 
