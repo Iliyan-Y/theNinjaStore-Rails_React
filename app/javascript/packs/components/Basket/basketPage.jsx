464d3f91c0b15a3620cbb511831e011cda279784
Iliyan-Y <ili.yankov91@gmail.com>
2020-11-25 16:24:10 +0000

    Basket styled

--- app/javascript/packs/components/Basket/basketPage.jsx
+++ app/javascript/packs/components/Basket/basketPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 import { useHistory } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const BasketPage = () => {
   let history = useHistory();
@@ -9,17 +10,57 @@ const BasketPage = () => {
 
   if (basket) {
     return (
-      <div>
-        {basket.items.map((each) => (
-          <span key={uuid()}>
-            <Link to={'/show/product/' + each.id}>{each.name}</Link>
-            <p>{each.description}</p>
-            <img src={each.image} alt="" style={{ width: '250px' }} />
-            <p>£{each.price}</p>
-            <br />
-          </span>
-        ))}
-        <button onClick={() => history.push('/order')}>Confirm Order</button>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <span
+          style={{
+            display: 'flex',
+            alignSelf: 'center',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {basket.items.map((each) => (
+            <span
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                textAlign: 'center',
+                margin: '0 1em',
+              }}
+              key={uuid()}
+            >
+              <Link
+                className="h5"
+                style={{
+                  fontFamily: 'Gill Sans, sans-serif',
+                  maxWidth: '180px',
+                }}
+                to={'/show/product/' + each.id}
+              >
+                {each.name}
+              </Link>
+              <img
+                src={each.image}
+                alt=""
+                style={{ width: '180px', margin: '0.3em 0' }}
+              />
+              <p>£{each.price}</p>
+              <br />
+            </span>
+          ))}
+        </span>
+        <Button
+          style={{ width: '165px', margin: '0 auto' }}
+          onClick={() => history.push('/order')}
+        >
+          Confirm Order
+        </Button>
       </div>
     );
   }
