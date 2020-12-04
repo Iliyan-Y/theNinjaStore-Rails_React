9d1eb6c2fedd3799cc4849db234c1567078e2c66
Iliyan-Y <ili.yankov91@gmail.com>
2020-12-04 11:03:05 +0000

    Refactured: renderitems style extracted under the component

--- app/javascript/packs/components/Basket/renderItems.jsx
+++ app/javascript/packs/components/Basket/renderItems.jsx
@@ -5,47 +5,18 @@ import RemoveItem from './removeItem';
 
 const RenderItems = ({ items, setBasket }) => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignSelf: 'center',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <div style={outerDiv}>
       {items.map((each) => (
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            textAlign: 'center',
-            margin: '0 1em',
-          }}
-          key={uuid()}
-        >
+        <div style={innerDiv} key={uuid()}>
           <Link
             className="h5"
-            style={{
-              fontFamily: 'Gill Sans, sans-serif',
-              maxWidth: '180px',
-            }}
+            style={productTitle}
             to={'/show/product/' + each.id}
           >
             {each.name}
           </Link>
-          <img
-            src={each.image}
-            alt={each.name}
-            style={{ width: '180px', margin: '0.3em 0' }}
-          />
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-          >
+          <img src={each.image} alt={each.name} style={coverPhoto} />
+          <div style={bottomInnerDiv}>
             <p>£{each.price}</p>
             <RemoveItem itemId={each.id} items={items} setBasket={setBasket} />
           </div>
@@ -56,3 +27,31 @@ const RenderItems = ({ items, setBasket }) => {
 };
 
 export default RenderItems;
+
+let outerDiv = {
+  display: 'flex',
+  alignSelf: 'center',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+let innerDiv = {
+  display: 'flex',
+  flexDirection: 'column',
+  textAlign: 'center',
+  margin: '0 1em',
+};
+
+let productTitle = {
+  fontFamily: 'Gill Sans, sans-serif',
+  maxWidth: '180px',
+};
+
+let bottomInnerDiv = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+};
+
+let coverPhoto = { width: '180px', margin: '0.3em 0' };
