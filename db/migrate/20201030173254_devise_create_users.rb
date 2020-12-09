c607e5d17541991ff5b1828a6570da1d41ae5cfc
Iliyan-Y <maximizebg@gmail.com>
2020-12-09 10:01:57 +0000

    Admin column added for the user db

--- db/migrate/20201030173254_devise_create_users.rb
+++ db/migrate/20201030173254_devise_create_users.rb
@@ -35,6 +35,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
       t.timestamps null: false
       t.string :auth_token
+      t.boolean :admin, default: false
     end
 
     add_index :users, :email,                unique: true
