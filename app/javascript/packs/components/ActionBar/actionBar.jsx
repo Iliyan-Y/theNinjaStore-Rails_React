0fceb7d50e28ecc065ece5c77865ba0187a74c87
Iliyan-Y <ili.yankov91@gmail.com>
2020-12-22 19:44:08 +0000

    checkout end point refactured to include different colors

--- app/javascript/packs/components/ActionBar/actionBar.jsx
+++ app/javascript/packs/components/ActionBar/actionBar.jsx
@@ -6,7 +6,6 @@ import { checkForUser } from '../../helpers/checkForUser';
 import { Navbar } from 'react-bootstrap';
 import NavBrand from './navBrand';
 import Menu from './menu';
-import axios from 'axios';
 
 const ActionBar = () => {
   const [cookies, setCookie, removeCookie] = useCookies();
@@ -22,13 +21,6 @@ const ActionBar = () => {
     setIsUser(false);
   };
 
-  const checkPay = () => {
-    axios
-      .get('/api/v1/orders/status')
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err.message));
-  };
-
   return (
     <Navbar bg="light" expand="lg" sticky="top">
       <NavBrand />
@@ -37,7 +29,6 @@ const ActionBar = () => {
         <Menu isAdmin={isUser.admin} isUser={isUser.user} logOut={logOut} />
         <BasketShortcut />
       </Navbar.Collapse>
-      <button onClick={() => checkPay()}>Check me</button>
     </Navbar>
   );
 };
