3621c31fd4c21dc411f2a386d2cd3aac83afe344
Iliyan-Y <maximizebg@gmail.com>
2021-01-09 16:59:50 +0000

    delete product component tests added

---
+++ spec/javascript/packs/components/Product/deleteProduct.spec.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render, waitFor, cleanup } from '@testing-library/react';
+import axios from 'axios';
+import DeleteProduct from 'packs/components/Product/deleteProduct';
+
+jest.mock('axios', () => ({
+  delete: jest.fn(),
+}));
+
+test('render the delete button', () => {
+  const { getByText } = render(<DeleteProduct />);
+  expect(getByText('Delete')).toBeInTheDocument();
+});
+
+test('click the delete button triggers axios delete request', async () => {
+  axios.delete.mockResolvedValue({});
+  const { getByText } = render(<DeleteProduct />);
+  let delBtn = getByText('Delete');
+  await waitFor(() => fireEvent.click(delBtn));
+  expect(axios.delete).toHaveBeenCalledTimes(1);
+});
