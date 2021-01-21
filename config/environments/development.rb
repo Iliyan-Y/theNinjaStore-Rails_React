013bddd83ceefe8e764fec818e751752352666ef
Iliyan-Y <ili.yankov91@gmail.com>
2021-01-21 15:47:23 +0000

    AWS SES set for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,10 +33,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Action mailer config
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
-  # config.action_mailer.delivery_url_options = {host: 'localhost', port: 3000}
   config.action_mailer.delivery_method = :smtp
   # AWS SES
   config.action_mailer.smtp_settings = {
