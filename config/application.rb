bba3ed660e48bd39e4cfd131eb5371c97e86c577
Iliyan-Y <ili.yankov91@gmail.com>
2021-01-24 17:50:41 +0000

    link added to the second app

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module TheNinjaStore
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'http://localhost:4567'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
